7f182e5c0aa7398401388456328729f837b79d00
marta <marta.habdas@sondasports.com>
2019-06-19 15:33:22 +0200

    add redux

--- index.js
+++ index.js
@@ -5,5 +5,20 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
 
-AppRegistry.registerComponent(appName, () => App);
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import promiseMiddleware from 'redux-promise'
+
+import reducers from './src/store/reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const createStoreWithMiddleware = createStore(reducers, composeEnhancers(
+    applyMiddleware(promiseMiddleware)
+));
+
+const appRedux = () => <Provider store={createStoreWithMiddleware}><App /></Provider>;
+
+AppRegistry.registerComponent(appName, () => appRedux);
