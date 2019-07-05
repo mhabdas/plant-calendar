7e5c2927b53a9c3a4813cc3e1ab7fee6f3f53303
marta <marta.habdas@gmail.com>
2019-07-05 12:55:05 +0200

    add firebase

---
+++ src/secrets/apiKey.js
@@ -0,0 +1,3 @@
+const SECRET_KEY = 'AIzaSyBPEUPwHP6su1iF2Pxjb4LTy2c_1C3pTkg';
+
+export default SECRET_KEY;
--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,13 +1,31 @@
 import axios from 'axios';
 import { SIGN_IN, SIGN_UP } from '../constants/user';
+import { SIGN_IN_URL, SIGN_UP_URL, FIREBASE_URL, REFRESH } from '../../utils/endpoints';
 
 const signUp = form => {
+  const request = axios({
+    method: 'POST',
+    url: SIGN_UP_URL,
+    data: {
+      email: form.email,
+      password: form.password,
+      returnSecureToken: true,
+    },
+    header: {
+      'Content-Type': 'application/json',
+    },
+  }).then(
+    response => {
+      return response.data;
+    },
+    error => {
+      console.log(error.data);
+    }
+  );
+
   return {
     type: SIGN_UP,
-    payload: {
-      email: form.email,
-      token: 'kjwkdkdjwkedjkewjdk',
-    },
+    payload: request,
   };
 };
 
